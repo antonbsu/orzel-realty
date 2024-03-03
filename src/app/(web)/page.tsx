0f06bd87786a3e4d084e77a1c221f36312456347
Aliaksandr Bandziuk <62721784+aliaksandr-bandziuk@users.noreply.github.com>
2024-03-03 23:33:51 +0100

    del rooms

--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -3,12 +3,8 @@ import LatestCommercialProperties from "@/components/LatestCommercialProperties/
 import LatestProperties from "@/components/LatestProperties/LatestProperties";
 import PageSearch from "@/components/PageSearch/PageSearch";
 import Reviews from "@/components/Reviews/Reviews";
-import { getFeaturedRoom } from "@/libs/apis";
 
 const Home = async () => {
-
-  const featuredRoom = await getFeaturedRoom();
-  // console.log(featuredRoom);
 
   return (
     <>
