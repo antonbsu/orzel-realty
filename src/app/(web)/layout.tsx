b4276a07e1945184e7d907f4c65f6edd8764913b
Aliaksandr Bandziuk <62721784+aliaksandr-bandziuk@users.noreply.github.com>
2024-03-10 19:57:59 +0100

    edit search and slider

--- src/app/(web)/layout.tsx
+++ src/app/(web)/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="pl">
       <head>
         <link
           rel='stylesheet'
