95826e0d59fe83fcc996ae9116c9ad65a77e2e81
Aliaksandr Bandziuk <62721784+aliaksandr-bandziuk@users.noreply.github.com>
2024-03-27 22:01:36 +0100

    change icon marker

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,8 +1,18 @@
 'use client';
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
 import "leaflet-defaulticon-compatibility";
+
+// import logoMarker from "../../images/house-icon.png";
+
+const customMarkerIcon = new L.Icon({
+    iconUrl: '../../images/logo.webp',
+    iconSize: [55, 55], // Размеры изображения маркера. Можете изменить под свои нужды
+    iconAnchor: [12, 41], // Точка, относительно которой иконка будет выровнена к координате карты
+    popupAnchor: [1, -34], // Точка, откуда будет "вырастать" всплывающее окно
+});
 
 const Map = () => {
   return (
@@ -18,6 +28,7 @@ const Map = () => {
       />
       <Marker
         position={[52.2275, 20.9541]}
+        icon={customMarkerIcon}
         draggable={false}
       >
         <Popup>To jest nasze biuro</Popup>
